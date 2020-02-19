9e977173cd27783476532ee2eb12d74e49d54ebd
codacy20 <kmuc72@gmail.com>
2020-02-20 00:13:04 +0100

    basic page structure

--- src/components/landing/Landing.tsx
+++ src/components/landing/Landing.tsx
@@ -9,15 +9,28 @@ export class Landing extends Component {
   render() {
     return (
       <Container>
-        <Row>
-          <Col xl>search box goes here</Col>
-        </Row>
-        <Row>
-          <Col xl>poll and stats goes here</Col>
-        </Row>
-        <Row>
-          <Col xl>footer goes here</Col>
-        </Row>
+        <section id='section1'>
+          <h1 id="heroTitle">Mix. Drink. Repeat.</h1>
+          <Row className='justify-content-md-center section1'>
+            <Col md="auto">
+              <Searchbox />
+            </Col>
+          </Row>
+        </section>
+        <section id='section2'>
+          <Row className='justify-content-md-center section2'>
+            <Col md="auto">
+              footer goes here
+          </Col>
+          </Row>
+        </section>
+        <section id='section3'>
+          <Row className='justify-content-md-center section3'>
+            <Col md="auto">
+              footer goes here
+          </Col>
+          </Row>
+        </section>
       </Container>
     )
   }
--- src/components/searchbox/Searchbox.tsx
+++ src/components/searchbox/Searchbox.tsx
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import './Searchbox.css';
+import { GoSearch } from "react-icons/go";
 
 export class Searchbox extends Component {
     render() {
         return (
-            <h1>Yo YO YO</h1>
+            <div className="search-wrapper active">
+                <div className="input-holder">
+                    <input type="text" className="search-input" placeholder="Type to search" />
+                    <button className="search-icon">
+                        <GoSearch />
+                    </button>
+                </div>
+                <div className="result-container">
+                </div>
+            </div>
         );
     }
 }
