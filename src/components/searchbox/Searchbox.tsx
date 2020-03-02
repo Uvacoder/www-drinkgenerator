8a5c1ca78415284f929435fbea1d335136a04333
codacy20 <kmuc72@gmail.com>
2020-03-02 21:47:39 +0100

    basic fetch for drinks

--- src/components/searchbox/Searchbox.tsx
+++ src/components/searchbox/Searchbox.tsx
@@ -9,17 +9,19 @@ export class Searchbox extends Component<{}, { is_visible: boolean }> {
 
     constructor(props: any) {
         super(props);
+        this.keyPress = this.keyPress.bind(this);
     }
 
-    componentDidMount() {
-        this.service.CocktailByName('gin');
+    keyPress(event: any) {
+        console.log('event value', event.target.value)
+        this.service.CocktailByName(event.target.value);
     }
 
     render() {
         return (
             <div className="search-wrapper active">
                 <div className="input-holder">
-                    <input type="text" className="search-input" placeholder="Type to search" />
+                    <input type="text" className="search-input" placeholder="Type to search" ref="SearchBox" onKeyUp={this.keyPress} />
                     <button className="search-icon">
                         <GoSearch />
                     </button>
